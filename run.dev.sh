e06f8109a5fe751e2ae8dcb77498f68041d7c244
jackin.chen <jackin.chen@huolala.cn>
2025-09-20 10:26:57 +0800

    调试站内搜索

--- run.dev.sh
+++ run.dev.sh
@@ -2,5 +2,4 @@
 
 # 启动 Hugo 本地开发服务器
 # shellcheck disable=SC2164
-cd exampleSite || exit
-hugo server --theme=WebStack-Hugo --config config.toml  --port 64341
+hugo server --theme=WebStack-Hugo --config config.toml  --port 64341 --buildDrafts
